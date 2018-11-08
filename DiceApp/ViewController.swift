bb662aaf89c2d0cfebcbd925ff29a2ad5f0ad751
Sam Blackburn <s.blackburn4@icloud.com>
2018-11-08 15:44:38 +0000

    Added the code to enable the dice to roll and display a random number each time

--- DiceApp/ViewController.swift
+++ DiceApp/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    let diceImageArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    var randomDice1 : Int = 0
+    var randomDice2 : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        rollDice()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        rollDice()
+    }
+    
+    func rollDice() {
 
+        randomDice1 = Int(arc4random_uniform(6))
+        randomDice2 = Int(arc4random_uniform(6))
+        
+        diceImageView1.image = UIImage(named: diceImageArray[randomDice1])
+        diceImageView2.image = UIImage(named: diceImageArray[randomDice2])
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        rollDice()
+    }
+    
 }
 
